3c4c11f25c1e77c9952748a655243f0216659f2e
DESKTOP-7ONKQ8V\Wich <wichersleyer@gmail.com>
2018-01-31 16:26:13 +0700

    test

---
+++ resources/views/match.blade.php
@@ -0,0 +1,22 @@
+     <!-- <h3>Profile</h3>
+     First name: <input type="text" name="fname"><br><br>
+     Last name: <input type="text" name="lname"><br><br>
+     Nick name: <input type="text" name="lname"><br><br>
+     Sex: <input type="radio" name="gender" value="male"> Male
+     <input type="radio" name="gender" value="female"> Female<br><br>
+     Age <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select> -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  	<link href="{{asset('css/style.css')}}" rel="stylesheet" />
+<div class="card">
+  <img src=https://www.shareicon.net/data/2016/07/26/802011_man_512x512.png alt="John" style="width:30%">
+  <h1>John Snow</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Harvard University</p>
+  
+  <p><button>Contact</button></p>
+</div>
