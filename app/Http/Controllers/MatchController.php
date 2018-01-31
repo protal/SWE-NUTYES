8b989ee6bb132e6406b7e8900c71180ab463da66
unknown <owen1491@gmail.com>
2018-01-31 15:19:04 +0700

    match algo

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -7,23 +7,79 @@ use Illuminate\Http\Request;
 class MatchController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
+
+      // $user = ['name' => 'Somthong' , "age" => "23" , "food" => "thai" , "music" => "rock" , "pet" => "cat" , "work" => "engineer" , "lifestyle" => "night" ];
+      $user['name'] = $request->input('nicname');
+      $user['firstname'] = $request->input('name');
+      $user['lastname'] = $request->input('lastname');
+      $user['sex'] = $request->input('sex');
+      $user['food'] = $request->input('food');
+      $user['sex'] = $request->input('sex');
+      $user['music'] = $request->input('music');
+      $user['pet'] = $request->input('pet');
+      $user['work'] = $request->input('work');
+      $user['lifestyle'] = $request->input('lifestyle');
+      $user['age'] = $request->input('age');
+
+      echo "<h1>NOW USER :</h1><br>";
+      echo " name : ".$user['name'];
+      echo " firstname : ".$user['firstname'];
+      echo " lastname : ".$user['lastname'];
+      echo " sex : ".$user['sex'];
+      echo " food : ".$user['food'];
+      echo " music : ".$user['music'];
+      echo " pet : ".$user['pet'];
+      echo " work : ".$user['work'];
+      echo " lifestyle : ".$user['lifestyle'];
+      echo " age : ".$user['age'];
+      echo "<br>";
+
       $users = [
-        ['name' => 'test' , "age" => "30" ],
-        ['name' => 'test1' , "age" => "30" ],
-        ['name' => 'test2' , "age" => "30" ],
-        ['name' => 'test3' , "age" => "30" ],
-        ['name' => 'test4' , "age" => "30" ],
-        ['name' => 'test5' , "age" => "30" ],
-        ['name' => 'test6' , "age" => "30" ],
+        ['name' => 'Sompong' ,'firstname' => 'Sompong' , 'lastname' => 'thongdee' , "sex" => "female" , "age" => "24" , "food" => "thai" , "music" => "rock" , "pet" => "cat" , "work" => "engineer" , "lifestyle" => "night" ],
+        ['name' => 'thu' , 'firstname' => 'Prayut' , 'lastname' => 'chanocha' , "sex" => "male" , "age" => "27" , "food" => "thai" , "music" => "pop" , "pet" => "dog" , "work" => "president" , "lifestyle" => "day" ],
+        ['name' => 'pom' , 'firstname' => 'Prawit' ,'lastname' => 'wongsuwan' , "sex" => "male" , "age" => "34" , "food" => "japan" , "music" => "rock" , "pet" => "cat" , "work" => "president" , "lifestyle" => "night" ],
       ];
 
       // dd($users);
-
+      echo "<h1>MATCH USER :</h1><br>";
       foreach ($users as $u) {
-        echo $u['name'];
-        echo "<br>";
+        $sum =0;
+        echo " Name : ".$u['name'];
+        echo " Firstname : ".$u['firstname'];
+        echo " Lastname : ".$u['lastname'];
+        echo " Sex : ".$u['sex'];
+        echo " Food : ".$u['food'];
+        echo " Music : ".$u['music'];
+        echo " Pet : ".$u['pet'];
+        echo " Work : ".$u['work'];
+        echo " Lifestyle : ".$u['lifestyle'];
+        echo " Age : ".$u['age'];
+
+        if($u["sex"] != $user["sex"]){
+          $sum++;
+        }
+        if($u["age"] == $user["age"]){
+          $sum++;
+        }
+        if($u["food"] == $user["food"]){
+          $sum++;
+        }
+        if($u["music"] == $user["music"]){
+          $sum++;
+        }
+        if($u["pet"] == $user["pet"]){
+          $sum++;
+        }
+        if($u["work"] == $user["work"]){
+          $sum++;
+        }
+        if($u["lifestyle"] == $user["lifestyle"]){
+          $sum++;
+        }
+      $percent = ($sum/6)*100 ;
+      echo "score ". number_format($percent, 2, '.', '')."%<br>";
       }
     }
 }
